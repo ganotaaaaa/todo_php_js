d42ecd201d466fce1dd8c608c5f56954df3f1a88
MYCOMPUTER\jakap <ys.training210629@gmail.com>
2024-04-08 19:26:27 +0900

    ウェルカムページの追加

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>simple_todo</title>
+    <link rel="stylesheet" href="./css/destyle.css">
+    <link rel="stylesheet" href="./css/home.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <main>
+        <div class="home-container">
+            <div class="home-title-container">
+                <h1 class="home-title">simple_todo</h1>
+            </div>
+            <div class="home-bottom-container">
+                <p class="home-caption">タスク管理が、もっと簡単に。<br>日々の生活をより豊かに。</p>
+                <a class="home-btn" href="login/">使ってみる</a>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
